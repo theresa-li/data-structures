aaa826f7f753c11f639912f9b032797678179017
theresa-li <tli2@cca.edu>
2019-02-20 11:47:44 -0800

    Complete pseudocode

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,17 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Check for key w/ largest number
+    // Create key (one more than largest number) = value
   };
 
   someInstance.pop = function() {
+    // Check for key w/ largest num
+    // Rm key from storage.
   };
 
   someInstance.size = function() {
+    // For each key in storage add to count
   };
 
   return someInstance;
