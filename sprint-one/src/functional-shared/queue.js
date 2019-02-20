72106ca672af586e0ba9140cdfffaf11e9bee176
theresa-li <tli2@cca.edu>
2019-02-20 15:19:33 -0800

    Write psuedocode

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  
+  _.extend(newQueue, queueMethods);
+  
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  // Find largest num in obj
+  // Add key (largest num + 1) = value
+};
 
+queueMethods.dequeue = function() {
+  // Find smallest num in object
+  // Save instance of key (smallest num) val
+  // Remove key
+  // Return instance
+};
+
+queueMethods.size = function() {
+  // Add to count for each key in obj
+};
