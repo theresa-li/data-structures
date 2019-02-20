b4efad3e6ab7ca624d54c46f490f687831454930
theresa-li <tli2@cca.edu>
2019-02-20 15:30:16 -0800

    Write code.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -11,17 +11,38 @@ var Queue = function() {
 var queueMethods = {};
 
 queueMethods.enqueue = function(value) {
+  var lgest;
   // Find largest num in obj
+  for (var key in this) {
+    if (lgest < key) {
+      lgest = key;
+    }
+  }
   // Add key (largest num + 1) = value
+  this[lgest + 1] = value;
 };
 
 queueMethods.dequeue = function() {
+  var smest;
   // Find smallest num in object
+  for (var key in this) {
+    if (smest === undefined || smest > key) {
+      smest = key;
+    }
+  }
   // Save instance of key (smallest num) val
+  var dequeued = this[smest];
   // Remove key
+  delete this[smest];
   // Return instance
+  return dequeued;
 };
 
 queueMethods.size = function() {
   // Add to count for each key in obj
+  var count = 0;
+  for (var key in this) {
+    count++;
+  }
+  return count;
 };
