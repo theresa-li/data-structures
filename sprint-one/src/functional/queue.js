64dd5dec5724549a084c42d0bbfd8332efa75440
theresa-li <tli2@cca.edu>
2019-02-20 12:24:32 -0800

    Use same code from stack.js for someInstance.size. Write psuedocode for someInstance.enqueue and someInstance.dequeue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,21 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // look for largest num key
+    // create key (num + 1) = value
   };
 
   someInstance.dequeue = function() {
+    // remove first item in key
   };
 
   someInstance.size = function() {
+    var count = 0;
+    // For each key in storage add to count
+    for (var key in storage) {
+      count++;
+    }
+    return count;
   };
 
   return someInstance;
