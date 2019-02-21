a40a447ed6cf6f4496d7cdb472ddb2fe7526ff55
theresa-li <tli2@cca.edu>
2019-02-20 18:21:13 -0800

    Debug code. Complete code.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,7 +1,6 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = {}; // this
   _.extend(someInstance, stackMethods);
   
   someInstance.storage = {};
