60da1fab110f318d702c31940d717f77153ec01a
theresa-li <tli2@cca.edu>
2019-02-20 20:20:32 -0800

    Debug code. Complete code.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,7 +2,7 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {}; 
-  _.extend(someInstance, stackMethods);
+  _.extend(someInstance, queueMethods);
   
   someInstance.storage = {};
   
