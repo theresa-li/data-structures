d9487c93643b8ffc54f27bfc88cb01f462e43ea4
Theresa <tli2@cca.edu>
2019-02-21 00:01:01 -0800

    Write pseudocode.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,20 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(string) {
+    // Add key (storage size) with value (string)
+  }
+
+  pop() {
+    // Store key (storage size - 1)
+    // Delete key
+    // Return key
+  }
+
+  size() {
+    // For each key in storage, add count
+  }
 }
