b59cadc4f326894436de71459667f48fd3d782bf
Theresa <tli2@cca.edu>
2019-02-21 00:14:34 -0800

    Write code.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -6,18 +6,39 @@ class Queue {
   }
 
   enqueue(string) {
+    var lgestNum = 0;
     // Find key with largest number
+    for (var key in this.storage) {
+      if (key > lgestNum) {
+        lgestNum = key;
+      }
+    }
     // Add key (largest number + 1) with value (string)
+    this.storage[lgestNum + 1] = string;
   }
 
   dequeued() {
+    var smestNum;
     // Find key with smallest number
+    for (var key in this.object) {
+      if (smestNum === undefined || smestNum > key){
+        smestNum = key;
+      }
+    }
     // Store key
+    var dequeued = this.storage[smestNum];
     // Delete key
+    delete this.storage[smestNum];
     // Return key
+    return dequeued;
   }
 
   size() {
+    var count = 0;
     // For each key in storage, add to count
+    for (var key in this.storage) {
+      count++;
+    }
+    return count;
   }
 }
