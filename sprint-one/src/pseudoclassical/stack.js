599699ced3c2f1f7f7083f732defc4a95b7a2688
Theresa <tli2@cca.edu>
2019-02-20 23:42:23 -0800

    Complete code.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -6,14 +6,23 @@ var Stack = function() {
 
 Stack.prototype.push = function(string) {
   // Add key (storage size) with value (string)
+  this.storage[this.size()] = string;
 };
 
 Stack.prototype.pop = function() {
   // Store key (storage size - 1) value
+  var popped = this.storage[this.size() - 1];
   // Delete key
+  delete this.storage[this.size() - 1];
   // Return stored key
+  return popped;
 };
 
 Stack.prototype.size = function() {
+  var count = 0;
   // For each key in storage, add to count
+  for (var key in this.storage) {
+    count++;
+  }
+  return count;
 };
