64c51281af8bb4eb1367e44b3b4b4eff8efbab66
Theresa <tli2@cca.edu>
2019-02-21 00:03:26 -0800

    Complete code.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -7,15 +7,24 @@ class Stack {
 
   push(string) {
     // Add key (storage size) with value (string)
+    this.storage[this.size()] = string;
   }
 
   pop() {
     // Store key (storage size - 1)
+    var popped = this.storage[this.size() - 1];
     // Delete key
+    delete this.storage[this.size() - 1];
     // Return key
+    return popped;
   }
 
   size() {
+    var count = 0;
     // For each key in storage, add count
+    for (var key in this.storage) {
+      count++;
+    }
+    return count;
   }
 }
