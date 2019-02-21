8222a44c8362215f12fc801e90aaa02ec5001b88
Theresa <tli2@cca.edu>
2019-02-20 23:38:44 -0800

    Write pseudocode

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(string) {
+  // Add key (storage size) with value (string)
+};
 
+Stack.prototype.pop = function() {
+  // Store key (storage size - 1) value
+  // Delete key
+  // Return stored key
+};
+
+Stack.prototype.size = function() {
+  // For each key in storage, add to count
+};
