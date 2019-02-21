c82ed7c2e9a80740a88d3eff9d031a6006c828a3
Theresa <tli2@cca.edu>
2019-02-20 23:53:02 -0800

    Complete code.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -5,17 +5,38 @@ var Queue = function() {
 };
 
 Queue.prototype.enqueue = function(string) {
+  var lgestNum = 0;
   // Find largest key number
+  for (var key in this.storage) {
+    if (key > lgestNum) {
+      lgestNum = key;
+    }
+  }
   // Add key (largest number + 1) with value (string)
+  this.storage[lgestNum + 1] = string;
 }
 
 Queue.prototype.dequeue = function() {
+  var smestNum;
   // Find smallest number
+  for (var key in this.storage) {
+    if (smestNum === undefined || smestNum > key) {
+      smestNum = key;
+    }
+  }
   // Store key (smallest number)
+  var dequeued = this.storage[smestNum];
   // Remove key
+  delete this.storage[smestNum];
   // Return key
+  return dequeued;
 }
 
 Queue.prototype.size = function() {
+  var count = 0;
   // For each key in storage, add count
+  for (var key in this.storage) {
+    count++;
+  }
+  return count;
 }
