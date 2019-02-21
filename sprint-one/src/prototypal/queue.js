ae8ee538bd95e0e30ed1a97a713fa847bfeb4fca
Theresa <tli2@cca.edu>
2019-02-20 23:32:34 -0800

    Complete code.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -3,7 +3,7 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = Object.create(queueMethods);
 
-  var storage = {};
+  someInstance.storage = {};
 
   return someInstance;
 };
@@ -11,17 +11,38 @@ var Queue = function() {
 var queueMethods = {};
 
 queueMethods.enqueue = function(string) {
+  var lgestNum = 0;
   // Find key with largest number
+  for (var key in this.storage) {
+    if (key > lgestNum) {
+      lgestNum = key;
+    }
+  }
   // Add key (largest number + 1) with value string
+  this.storage[lgestNum + 1] = string;
 }
 
 queueMethods.dequeue = function() {
+  var smestNum;
   // Find key with smallest number
+  for (var key in this.storage) {
+    if (smestNum === undefined || smestNum > key) {
+      smestNum = key;
+    }
+  }
   // Store key (smallest number)
+  var dequeued = this.storage[smestNum];
   // Remove key
+  delete this.storage[smestNum];
   // Return key
+  return dequeued;
 }
 
 queueMethods.size = function() {
+  var count = 0;
   // For each key in storage, add to count
+  for (var key in this.storage) {
+    count++;
+  }
+  return count;
 }
