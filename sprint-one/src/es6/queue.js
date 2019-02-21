7e5d52870e38e581d1dc29a604ef9c189e4a1987
Theresa <tli2@cca.edu>
2019-02-21 00:07:48 -0800

    Write pseudocode.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,22 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(string) {
+    // Find key with largest number
+    // Add key (largest number + 1) with value (string)
+  }
+
+  dequeued() {
+    // Find key with smallest number
+    // Store key
+    // Delete key
+    // Return key
+  }
+
+  size() {
+    // For each key in storage, add to count
+  }
 }
